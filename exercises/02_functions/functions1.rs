e1f8d7f16834bbeafc13e8453a310c225526ea81
Risang Baskoro <contact@risangbaskoro.com>
2025-02-24 00:02:00 +0700

    Functions

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,6 @@
-// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    println!("Function `call_me` is called.")
+}
 
 fn main() {
     call_me(); // Don't change this line
