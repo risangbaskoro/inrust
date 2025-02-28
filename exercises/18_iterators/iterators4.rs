5db6d4e111b5acfb7edddeefcfff2134a820cf48
Risang Baskoro <contact@risangbaskoro.com>
2025-03-01 02:08:35 +0700

    Iterators

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,8 +1,4 @@
 fn factorial(num: u64) -> u64 {
-    // TODO: Complete this function to return the factorial of `num` which is
-    // defined as `1 * 2 * 3 * … * num`.
-    // https://en.wikipedia.org/wiki/Factorial
-    //
     // Do not use:
     // - early returns (using the `return` keyword explicitly)
     // Try not to use:
@@ -10,6 +6,7 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+    (1..=num).reduce(|acc, item| acc * item).unwrap_or(1)
 }
 
 fn main() {
